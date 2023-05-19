30b2b381e5b619359b84840ccdf8fb26ba3a4989
sanji2207 <saepullohepul565@gmail.com>
2023-05-19 23:45:26 +0700

    hello

---
+++ example/nama_library_example.dart
@@ -0,0 +1,6 @@
+import 'package:nama_library/nama_library.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/nama_library.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library nama_library;
+
+export 'src/nama_library_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
